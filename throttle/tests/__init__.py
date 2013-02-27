c3a9d24a39d849724bb94ca9f3cfe5b8461c18c9
Lewis <lewis@semadic.com>
2013-02-27 09:35:00 -0800

    Fix for Python 2.5

--- throttle/tests/__init__.py
+++ throttle/tests/__init__.py
@@ -1,4 +1,4 @@
-from .test_utils import test_load_module_from_path
-from .test_zones import TestRemoteIP, Test_ThrottleZone
-from .test_decorators import test_throttle
-from .backends import *
+from throttle.tests.test_utils import test_load_module_from_path
+from throttle.tests.test_zones import TestRemoteIP, Test_ThrottleZone
+from throttle.tests.test_decorators import test_throttle
+from throttle.tests.backends import *

f616b60b184083283e3db3a1fb274e464b9049d2
Lewis Sobotkiewicz <lsobotkiewicz@kixeye.com>
2015-04-26 23:39:41 -0700

    Fixed broken release.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import find_packages, setup
 try:
     import multiprocessing # Suppresses a confusing but harmless warning when running ./setup.py test
     import logging
@@ -16,7 +16,7 @@ setup(
     name="django-throttle-requests",
     description="A Django framework for application-layer rate limiting",
     long_description=long_description,
-    packages=['throttle'],
+    packages=find_packages(),
     url="https://github.com/sobotklp/django-throttle-requests",
     version=__version__,
     author='Lewis Sobotkiewicz',
@@ -25,7 +25,6 @@ setup(
         'Django>=1.3.7',
     ],
     test_suite='runtests.runtests',
-    use_2to3=True,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
--- throttle/__init__.py
+++ throttle/__init__.py
@@ -1,3 +1,3 @@
-__version_info__ = ('0','5','0')
+__version_info__ = ('0','5','2')
 __version__ = ".".join(__version_info__)
 

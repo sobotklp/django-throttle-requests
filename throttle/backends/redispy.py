5be2811975bfe20d635c72857d8c1db383db7bb8
Justin van Koten <jvankoten@users.noreply.github.com>
2018-05-15 13:34:27 -0700

    add ability to pass password to redis connection

--- throttle/backends/redispy.py
+++ throttle/backends/redispy.py
@@ -32,8 +32,9 @@ class RedisBackend(ThrottleBackendBase):
         THROTTLE_REDIS_HOST = getattr(settings, 'THROTTLE_REDIS_HOST', 'localhost')
         THROTTLE_REDIS_PORT = getattr(settings, 'THROTTLE_REDIS_PORT', 6379)
         THROTTLE_REDIS_DB = getattr(settings, 'THROTTLE_REDIS_DB', 0)
+        THROTTLE_REDIS_AUTH = getattr(settings, 'THROTTLE_REDIS_AUTH', None)
 
-        self.pool = redis.ConnectionPool(host=THROTTLE_REDIS_HOST, port=THROTTLE_REDIS_PORT, db=THROTTLE_REDIS_DB)  #TODO: Parameterize connection parameters
+        self.pool = redis.ConnectionPool(host=THROTTLE_REDIS_HOST, port=THROTTLE_REDIS_PORT, db=THROTTLE_REDIS_DB, password=THROTTLE_REDIS_AUTH)  #TODO: Parameterize connection parameters
 
     def incr_bucket(self, zone_name, bucket_key, bucket_num, bucket_num_next, bucket_span, cost=1):
         conn = redis.Redis(connection_pool=self.pool)
